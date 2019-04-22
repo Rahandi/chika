e4d00e46764d9085e8b7c479f64eb048392a4ddf
rahandi <rahandinoor@gmail.com>
2019-04-22 15:02:26 +0700

    add removebg

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 import sys
 import git
+import requests
 from dotenv import load_dotenv
+from imgurpython import ImgurClient
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageMessage, ImageSendMessage
 
 load_dotenv()
 
@@ -18,8 +20,46 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(os.environ['LINE_CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['LINE_CHANNEL_SECRET'])
 
+imgur_client = ImgurClient(
+    os.environ['IMGUR_CLIENT_ID'], 
+    os.environ['IMGUR_CLIENT_SECRET'],
+    os.environ['IMGUR_ACCESS_TOKEN'],
+    os.environ['IMGUR_REFRESH_TOKEN']
+)
+
+def getContent(msg_id):
+    message_content = line_bot_api.get_message_content(str(msg_id))
+    try:
+        os.mkdir('saved_content')
+    except:
+        pass
+    with open('saved_content/' + str(msg_id)) as f:
+        for chunk in message_content.iter_content():
+            f.write(chunk)
+    return 'saved_content/' + str(msg_id)
+
+def removebgAPI(path):
+    response = requests.post(
+        'https://api.remove.bg/v1.0/removebg',
+        files = {'image_file':open(path, 'rb')},
+        data = {'size':'auto'},
+        headers = {'X-API-Key':os.environ['REMOVEBG_KEY']}
+    )
+    path = path.split('/')
+    path[-1] = path[-1] + '_removebg'
+    path = '/'.join(path)
+    if response.status_code == 200:
+        with open('saved_contents', 'wb') as out:
+            out.write(response.content)
+    return path
+
 def restart():
     os.execv(sys.executable, ['python3']+sys.argv)
+
+def uploadToImgur(path):
+    response = imgur_client.upload_from_path(path)
+    link = response['link']
+    return link
 
 @app.route('/callback', methods=['POST'])
 def callback():
@@ -34,12 +74,8 @@ def callback():
 
     return 'OK'
 
-@app.route('/')
-def home():
-    return 'ok'
-
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
+def handle_text(event):
     token = event.reply_token
     message = event.message.text
     source = event.source.user_id
@@ -67,6 +103,15 @@ def handle_message(event):
         line_bot_api.reply_message(token, TextSendMessage(text='ok'))
     elif message == 'chika userId':
         line_bot_api.reply_message(token, TextSendMessage(text=str(source)))
+
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_image(event):
+    token = event.reply_token
+    message_id = event.message.id
+    path = getContent(message_id)
+    path = removebgAPI(path)
+    link = uploadToImgur(path)
+    line_bot_api.reply_message(token, ImageSendMessage(original_content_url=link, preview_image_url=link))
 
 @handler.default()
 def default(event):
